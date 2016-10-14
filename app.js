8fc590f3a8145192454bdaae803e4a09a7e1f08e
cognatus <diegojmartinezzm@gmail.com>
2016-10-13 20:53:14 -0500

    A ver que onda

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -21,7 +20,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//rutas
+
+app.get('/', routes.index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
